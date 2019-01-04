603d619baecf85a24a99ea3c7ec5a5efb7e03af6
Rob Guempel <rguempel@gmail.com>
2019-01-04 15:59:07 -0500

    created game logic

---
+++ memory_game/javascript/main.js
@@ -0,0 +1,7 @@
+//JavaScript file
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log ("User Flipped " + cardOne);
+console.log ("User Flipped " + cardThree);
