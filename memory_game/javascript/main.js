fc30dfd465cb5d956a3b3aac192849319d59bc22
Rob Guempel <rguempel@gmail.com>
2019-01-10 18:13:42 -0500

    created game logic

--- memory_game/javascript/main.js
+++ memory_game/javascript/main.js
@@ -1,7 +1,18 @@
-//JavaScript file
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log ("User Flipped " + cardOne);
-console.log ("User Flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2]	;
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " );
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!");
+	}
+	else {
+		alert("sorry, keep trying");
+	}
+}
