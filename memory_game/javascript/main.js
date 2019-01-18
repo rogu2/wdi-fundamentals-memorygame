7b6508bb6982150ebde54cb9256bfa34e9c028db
Rob Guempel <rguempel@gmail.com>
2019-01-17 21:24:23 -0500

    finished memory game

--- memory_game/javascript/main.js
+++ memory_game/javascript/main.js
@@ -2,66 +2,59 @@
 //var cards = ["queen", "queen", "king", "king"];
 
 var cards = [
-{
+	{
 	rank: "queen",
 	suit: "hearts",
 	cardImage: "images/queen-of-hearts.png"
-},
+	},
 
-{
+	{
 	rank: "queen",
 	suit: "diamonds",
 	cardImage: "images/queen-of-diamonds.png"
-},
+	},
 
-{
+	{
 	rank: "king",
 	suit: "hearts",
 	cardImage: "images/king-of-hearts.png"
-},
+	},
 
-{
+	{
 	rank: "king",
 	suit: "hearts",
 	cardImage: "images/king-of-diamonds.png"
-}
+	},
 ];
-
 
 var cardsInPlay = [];
 
-var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again.");
+function checkForMatch (id) {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
 
 function flipCard() {
-	var cardId;
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardID].rank);
-	console.log(cardImage);
-	console.log(suit);
-
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You've found a match!");
-		} else {
-			alert("Sorry, try again.");
-		}
-	};
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch(cardId);
 };
 
-flipCard(checkForMatch);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+}
 
-/*/////Old Code//////
-var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log('User flipped ' + cardsInPlay);
-
-	var cardTwo = cards[2];
-	cardsInPlay.push(cardTwo);
-	console.log('User flipped ' + cardsInPlay);
-*/
+createBoard();
